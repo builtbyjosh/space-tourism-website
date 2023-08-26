f9026b63085cf1a1bf944dc59e0c26f1569d6e21
builtbyjosh <joshuacollins912@gmail.com>
2023-08-26 13:37:56 -0500

    testing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Box, Text } from "@chakra-ui/react";
+import DesktopHeader from "./components/menu/DesktopHeader";
+import HomePage from "./components/pages/HomePage";
+import DesktopBG from "./assets/home/background-home-desktop.jpg";
+import DestinationsPage from "./components/pages/DestinationsPage";
 
 function App() {
   return (
-    <Box>
-      <Text>Space Tourism Website</Text>
+    <Box bgImage={DesktopBG} h={"100vh"} w={"full"}>
+      <DesktopHeader />
+      <HomePage />
+      <DestinationsPage />
     </Box>
   );
 }
