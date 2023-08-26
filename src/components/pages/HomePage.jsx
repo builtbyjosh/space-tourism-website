7ce677bee6dfbccb8d017c8a7bc5932c3cda467a
builtbyjosh <joshuacollins912@gmail.com>
2023-08-26 13:38:56 -0500

    start homepage

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, Stack, Box, Circle, Heading } from "@chakra-ui/react";
+import DesktopBG from "../../assets/home/background-home-desktop.jpg";
+const HomePage = () => {
+  return (
+    <Box h={"full"} w={"full"}>
+      <Stack direction={"row"}>
+        <Stack
+          direction={"column"}
+          textColor={"secondary"}
+          textStyle={"primary"}
+        >
+          <Text as="h5" fontSize="h5">
+            SO, YOU WANT TO TRAVEL TO
+          </Text>
+          <Text as="h1" fontSize="h1">
+            SPACE
+          </Text>
+          <Text>
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </Text>
+        </Stack>
+        <Circle>
+          <Text>EXPLORE</Text>
+        </Circle>
+      </Stack>
+    </Box>
+  );
+};
+
+export default HomePage;
