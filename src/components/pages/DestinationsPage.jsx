ebac4fb8ae24915e2e0af9b0851b1c1ccecae60d
builtbyjosh <joshuacollins912@gmail.com>
2023-08-29 10:28:16 -0500

    get planet image

--- src/components/pages/DestinationsPage.jsx
+++ src/components/pages/DestinationsPage.jsx
@@ -1,24 +1,38 @@
-import { Box, Stack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Box, Stack, Image } from "@chakra-ui/react";
 import DestinationLink from "../menu/DestinationLink";
 import { useSpaceContext } from "../../context/SpaceContext";
 import DestinationCard from "../cards/DestinationCard";
 
 const DestinationsPage = () => {
   const { collectionData, collectionIndex } = useSpaceContext();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    if (collectionData[collectionIndex]) {
+      setImage(collectionData[collectionIndex].images.png);
+    }
+  }, [collectionData[collectionIndex]]);
+
+  console.log("IMG URL:", image);
+
   return (
-    <Box>
-      <Stack direction={"row"}>
-        {collectionData &&
-          collectionData.map((data, index) => {
-            return (
-              <DestinationLink text={data.name} index={index} key={index} />
-            );
-          })}
+    <Stack direction={"row"}>
+      <Image src={image} />
+      <Stack direction={"column"}>
+        <Stack direction={"row"}>
+          {collectionData &&
+            collectionData.map((data, index) => {
+              return (
+                <DestinationLink text={data.name} index={index} key={index} />
+              );
+            })}
+        </Stack>
+        {collectionData[collectionIndex] && (
+          <DestinationCard destinationData={collectionData[collectionIndex]} />
+        )}
       </Stack>
-      {collectionData && (
-        <DestinationCard destinationData={collectionData[collectionIndex]} />
-      )}
-    </Box>
+    </Stack>
   );
 };
 
