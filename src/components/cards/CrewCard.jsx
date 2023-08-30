bef38ae165403778616499507c57c0d0402543eb
builtbyjosh <joshuacollins912@gmail.com>
2023-08-30 08:40:41 -0500

    add styling

--- src/components/cards/CrewCard.jsx
+++ src/components/cards/CrewCard.jsx
@@ -4,10 +4,10 @@ import { Box, Text } from "@chakra-ui/react";
 const CrewCard = ({ crewMemberData }) => {
   const { name, bio, role } = crewMemberData;
   return (
-    <Box>
-      <Text>{name}</Text>
+    <Box textColor={"secondary"} textStyle={"primary"}>
+      <Text fontSize={"h4"}>{role}</Text>
+      <Text fontSize={"h3"}>{name}</Text>
       <Text>{bio}</Text>
-      <Text>{role}</Text>
     </Box>
   );
 };
