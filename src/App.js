bf25817306f00a7dcaf316a514e5f8a7df28eb11
builtbyjosh <joshuacollins912@gmail.com>
2023-08-30 08:42:42 -0500

    add bg image logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import { Box } from "@chakra-ui/react";
 import DesktopHeader from "./components/menu/DesktopHeader";
 import HomePage from "./components/pages/HomePage";
-import DesktopBG from "./assets/home/background-home-desktop.jpg";
 import DestinationsPage from "./components/pages/DestinationsPage";
 import CrewPage from "./components/pages/CrewPage";
 import TechnologiesPage from "./components/pages/TechnologiesPage";
 import { useSpaceContext } from "./context/SpaceContext";
+import HomeBg from "./images/home/background-home-desktop.jpg";
+import CrewBg from "./images/crew/background-crew-desktop.jpg";
+import DestinationBg from "./images/destination/background-destination-desktop.jpg";
+import TechnologyBg from "./images/technology/background-technology-desktop.jpg";
 
 function App() {
-  const { collection } = useSpaceContext();
+  const { collection, collectionIndex } = useSpaceContext();
+
+  const collectionMap = {
+    home: HomeBg,
+    crew: CrewBg,
+    destination: DestinationBg,
+    technology: TechnologyBg,
+  };
+  const selectedBgImage = collectionMap[collection];
   return (
-    <Box bgImage={DesktopBG} h={"100vh"} w={"full"}>
+    <Box bgImage={selectedBgImage} h={"100vh"} w={"full"}>
       <DesktopHeader />
-      {collection === "home" && <HomePage />}
-      {collection === "destination" && <DestinationsPage />}
-      {collection === "crew" && <CrewPage />}
-      {collection === "technology" && <TechnologiesPage />}
+      <Box p={10}>
+        {collection === "home" && <HomePage />}
+        {collection === "destination" && <DestinationsPage />}
+        {collection === "crew" && <CrewPage />}
+        {collection === "technology" && <TechnologiesPage />}
+      </Box>
     </Box>
   );
 }
