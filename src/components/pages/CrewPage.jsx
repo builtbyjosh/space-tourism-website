c7aca142b2667febaa0436a1ed6ddd39bf8695fe
builtbyjosh <joshuacollins912@gmail.com>
2023-08-30 08:41:17 -0500

    change layout and fix image fetch

--- src/components/pages/CrewPage.jsx
+++ src/components/pages/CrewPage.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import { useSpaceContext } from "../../context/SpaceContext";
-import { Box } from "@chakra-ui/react";
+import { Box, Image, Stack } from "@chakra-ui/react";
 import CrewCard from "../cards/CrewCard";
 
 const CrewPage = () => {
   const { collectionData, collectionIndex } = useSpaceContext();
+  const [image, setImage] = useState();
 
+  useEffect(() => {
+    if (collectionData[collectionIndex]) {
+      setImage(collectionData[collectionIndex].images.png);
+    }
+  }, [collectionData[collectionIndex]]);
   return (
     <Box>
-      {collectionData && (
-        <CrewCard crewMemberData={collectionData[collectionIndex]} />
-      )}
+      <Stack direction={"row"} spacing={10}>
+        {collectionData && (
+          <CrewCard crewMemberData={collectionData[collectionIndex]} />
+        )}
+        <Image src={image} />
+      </Stack>
     </Box>
   );
 };
