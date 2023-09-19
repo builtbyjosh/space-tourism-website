70ecfb3546fe883d2c765c33791e890d381ded68
builtbyjosh <joshuacollins912@gmail.com>
2023-09-19 13:30:02 -0500

    add buttons to cycle crew members

--- src/components/pages/CrewPage.jsx
+++ src/components/pages/CrewPage.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useSpaceContext } from "../../context/SpaceContext";
-import { Box, Image, Stack } from "@chakra-ui/react";
+import { Box, Image, Stack, Button, ButtonGroup } from "@chakra-ui/react";
 import CrewCard from "../cards/CrewCard";
 
 const CrewPage = () => {
-  const { collectionData, collectionIndex } = useSpaceContext();
+  const { collectionData, collectionIndex, setCollectionIndex } =
+    useSpaceContext();
   const [image, setImage] = useState();
 
   useEffect(() => {
@@ -12,12 +13,28 @@ const CrewPage = () => {
       setImage(collectionData[collectionIndex].images.png);
     }
   }, [collectionData[collectionIndex]]);
+
   return (
     <Box>
-      <Stack direction={"row"} spacing={10}>
-        {collectionData && (
-          <CrewCard crewMemberData={collectionData[collectionIndex]} />
-        )}
+      <Stack direction={"row"} spacing={10} p={10}>
+        <Box>
+          {collectionData && (
+            <CrewCard crewMemberData={collectionData[collectionIndex]} />
+          )}
+          <ButtonGroup spacing={2}>
+            {[0, 1, 2, 3].map((index) => (
+              <Button
+                key={index}
+                borderRadius="full"
+                w="10px"
+                h="10px"
+                bg={collectionIndex === index ? "blue.500" : "gray.200"} // Change colors as needed
+                onClick={() => setCollectionIndex(index)}
+              />
+            ))}
+          </ButtonGroup>
+        </Box>
+
         <Image src={image} />
       </Stack>
     </Box>
